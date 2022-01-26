aa25916fd449d15f7864c5b8f45cbd477754002d
BECARIO D.WEB12 <cristianajquejay@gmail.com>
2022-01-26 12:36:07 -0600

    reenderización del lado del cliente

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', function(req, res) {
-            db.collection('items').find().toArray(function(err, items) {
-                        res.send(`<!DOCTYPE html>
+    db.collection('items').find().toArray(function(err, items) {
+        res.send(`<!DOCTYPE html>
       <html>
       <head>
         <meta charset="UTF-8">
@@ -50,18 +50,13 @@ app.get('/', function(req, res) {
           </div>
           
           <ul id="item-list" class="list-group pb-5">
-            ${items.map(function(item){
-              return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-              <span class="item-text">${item.text}</span>
-              <div>
-                <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-                <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
-              </div>
-            </li>`
-            }).join('')}
+       
           </ul>
           
         </div>
+        <script>
+        let items = ${JSON.stringify(items)}
+        </script>
         <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
         <script src="/browser.js"></script>
       </body>
@@ -70,19 +65,19 @@ app.get('/', function(req, res) {
 
 })
 app.post('/create-item', function(req, res) {
-  db.collection('items').insertOne({text: req.body.text}, function(err, info) {
-    res.json({_id: info.insertedId, text: req.body.text})
-  })
+    db.collection('items').insertOne({ text: req.body.text }, function(err, info) {
+        res.json({ _id: info.insertedId, text: req.body.text })
+    })
 })
 
 app.post('/update-item', function(req, res) {
-  db.collection('items').findOneAndUpdate({_id: new ObjectId(req.body.id)}, {$set: {text: req.body.text}}, function() {
-    res.send("Success")
-  })
+    db.collection('items').findOneAndUpdate({ _id: new ObjectId(req.body.id) }, { $set: { text: req.body.text } }, function() {
+        res.send("Success")
+    })
 })
 
 app.post('/delete-item', function(req, res) {
-  db.collection('items').deleteOne({_id: new ObjectId(req.body.id)}, function() {
-    res.send("Success")
-  })
+    db.collection('items').deleteOne({ _id: new ObjectId(req.body.id) }, function() {
+        res.send("Success")
+    })
 })
